9435d77e1872032e6740cb6cef977a8956011791
Meherun Nessa Ferdausi <70442441+meheroon@users.noreply.github.com>
2026-06-22 22:20:17 +0600

    Setup Firebase authentication provider

---
+++ app/lib/firebase.config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "temp",
+  authDomain: "temp",
+  projectId: "temp",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
